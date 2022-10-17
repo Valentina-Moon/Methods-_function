692105b50ac5778ea6f9c2fc57ac8ea9b6adcaa6
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-17 22:20:48 +0300

    +*

---
+++ Task7/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//модификатор(static) тип вазращаемого значения метода(параметры) 
+// {
+// тело метода   
+// }
+
+
+static int Sum(int a, int b)
+{
+    int result = a + b; // можно поменять логику выполнения a*b// valua-1 = a; valua_2= b
+
+
+    return result;
+}
+
+int a, b, c;
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+//c= a + b;
+int c = Sum(a, b);
+Console.WriteLine(c);
